d82b7953c32c26aaacbbfb9e7f7c06676bb20065
Niels van der meer <you@example.com>
2020-11-10 15:44:09 +0100

    python eindopdracht

---
+++ 5-eindopdracht/opdracht.py
@@ -0,0 +1,23 @@
+import time
+s = input("Sec: ")
+s = int(s)
+m = input("Min:")
+m = int(m)
+h = input("Hour:")
+h = int(h) 
+
+while (s < 60): # als seconden kleiner is dan 60 runt hij de print en doet er elke keer 1 bij en de seconden timer wordt gereset
+    print ("H:"+str(h) +" "+ "Min:"+ str(m)+" "+ "Sec:" + str(s)) 
+    time.sleep(1)
+    s=s+1
+    if (s == 60): #als s gelijk is aan 60 komt er +1 bij bij de minuut
+        m=m+1
+        s = 0
+
+    elif(m == 60): #als de minuut gelijk is aan 60 dan komt er +1 uur bij
+        h=h+1
+        m = 0
+        s = 0
+    
+    elif(h == 24): #als h gelijk is aan 24 reset uren en begint hij helemaal opnieuw
+        h = 0
